95e86abbf6f5f396bd934f45fe16db8d22c04d3c
barsu5489 <ebarsulai@gmail.com>
2023-02-07 10:45:21 +0300

    methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+puts "Hello, programmer!"
+end
+
+def greet name
+puts "Hello, #{name}!"
+end
+
+def greet_with_default name = "programmer!"
+    puts "Hello, #{name}!"
+end
+
+def add a,b
+    return a + b
+end
+
+def halve data
+    if data.is_a?Integer
+        return data / 2
+    else
+        return nil
+    end
+end
